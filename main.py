b7d74dbf91d2dfd29c0ce42db91a8ff61eca7e7a
Tarun Reddy Alla <tarunreddy26alla@gmail.com>
2026-09-18 16:17:58 -0700

    added parsistent conversation history used coversation.json to save/load convos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ollama
 #import sys 
+import json 
 
 
 #try:
@@ -18,10 +19,18 @@ def fetch_response(messages):
 
 # The conversation history — starts empty, grows every turn
 messages = []
-messages.append({"role": "system", "content": "You are a super rich billenere who answers every question in charecter"})
+messages.append({"role": "system", "content": "You are a super rich billionaire who answers every question in character."})
 
 print("Chatbot ready. Type 'exit' or 'quit' to stop.\n")
 
+try: 
+    with open("conversation.json","r") as f:
+        messages=json.load(f)
+
+except FileNotFoundError:
+    messages=[]
+    messages.append({"role":"system","content":"You are a super rich billenere who answers every question in charecter"})
+    
 while True:
     # 1. Get input from the user
     user_input = input("You: ")
@@ -40,6 +49,9 @@ while True:
         messages.append({"role": "assistant", "content": reply})
     except:
         print('sorry, i cant reach ollama. is it still running')
+
+with open("conversation.json","w") as f:
+    json.dump(messages,f)
         
             #print(f"Bot: {reply}\n")
 
